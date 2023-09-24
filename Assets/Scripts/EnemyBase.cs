05e69b8754b02288714bb2f7f5daadd8d5b5d3f5
Daniel Mupet <mupet.col@gmail.com>
2023-09-24 16:44:18 -0500

    Visual additions

--- Assets/Scripts/EnemyBase.cs
+++ Assets/Scripts/EnemyBase.cs
@@ -3,8 +3,9 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-    public class EnemyBase : MonoBehaviour
+public class EnemyBase : MonoBehaviour
     {
         public int hitPoints = 0;
         public int maxHitPoints = 5;
@@ -19,6 +20,7 @@ using UnityEngine;
         {
             if (hitPoints >= maxHitPoints)
             {
+                SceneManager.LoadScene(0);
                 Debug.Log("Player Won");
             }
         }
